fa013f1769eadd7b486a37cf66892e816f6ecb1d
betthical <diegongpro@gmail.com>
2022-11-01 18:32:49 +0100

    0.1

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+import string
+
+
+
+def letters(x):
+
+    lets=string.ascii_letters
+    abc=[]
+
+    for i in lets:
+        abc.append(i)
+
+
+
+def nums(y):
+
+    n=string.digits
+    numList=[]
+
+    for i in n:
+        numList.append(i)
+
+
+def symbols(z):
+    
+    symbs=string.punctuation
+    symbList=[]
+
+    for i in symbs:
+        symbList.append(i)
+
+
+
